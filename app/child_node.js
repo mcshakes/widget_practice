fa37a6c556864e46fe5899f48f3e50ff0a5e9e1a
Edward McCarthy <efmccarthy26@gmail.com>
2017-10-09 13:20:46 -0600

    removed comments

---
+++ app/child_node.js
@@ -0,0 +1,41 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+class ChildNode extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { childVisible: false }
+  }
+
+  render() {
+    let childnodes = null;
+
+    // if (this.props.children) {
+    //   childnodes = this.props.children.map((childnode) => {
+    //     return (
+    //       <ChildNode node={childnode} children={childnode.subfeatures} />
+    //     );
+    //   })
+    // }
+
+    if (this.state.childVisible) {
+      childnodes = (<ChildNode node={childnode} children={childnode.subfeatures} />)
+    }
+
+    return (
+      <div>
+        <li>
+          <span>
+            <a rel="{this.props.children.id}" onClick={this.handleCollapse.bind(this)}>{this.props.node.title}</a>
+          </span>
+          <input value="" type="checkbox" onClick={this.handleFilter.bind(this)} />
+          { childnodes ?
+            <ul>{childnodes}</ul>
+            : null }
+          </li>
+      </div>
+    )
+  }
+}
+
+export default ChildNode;
