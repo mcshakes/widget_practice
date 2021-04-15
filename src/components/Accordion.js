1479fe3b0db98e0a6b88a4f9bd896958d9472739
mcshakes <efmccarthy26@gmail.com>
2021-04-14 19:13:27 -0600

    removed crap and first hook in

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Accordion = ({ items }) => {
+    const [activeIndex, setActiveIndex] = useState(null)
 
-    const renderedItems = items.map(item => {
+    const onTitleClick = (index) => {
+        setActiveIndex(index);
+    }
+
+    const renderedItems = items.map((item, index) => {
         return (
             <React.Fragment key={item.title}>
-                <div className="title active">
+                <div 
+                    className="title active"
+                    onClick={() => onTitleClick(index)}
+                >
                     <i className="dropdown icon"></i>
                         {item.title}
                 </div>
