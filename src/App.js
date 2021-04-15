fd1ef197f7b2ca07ff03f11d09631c775ee08fdc
mcshakes <efmccarthy26@gmail.com>
2021-04-14 18:56:11 -0600

    semantic UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from "react";
 import Accordion from "./components/Accordion";
 
+const items = [
+    {
+        title: "What is love?",
+        content: "A man made construct"
+    },
+    {
+        title: "Why use React?",
+        content: "Cool, I guess..."
+    },
+    {
+        title: "What happens after death?",
+        content: "Nothing..."
+    }
+]
+
 const App = () => {
     return (
         <div>
-            <Accordion />
+            <Accordion items={items}/>
         </div>
     )
 }
--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,9 +1,25 @@
 import React from "react";
 
-const Accordion = () => {
+const Accordion = ({ items }) => {
+
+    const renderedItems = items.map(item => {
+        return (
+            <div key={item.title}>
+                <div className="title active">
+                    <i className="dropdown icon">
+                        {item.title}
+                    </i>
+                </div>
+                <div className="content active">
+                    <p>{item.content}</p>
+                </div>
+            </div>
+        )
+    });
+
     return (
-        <div>
-            <h1>Am Accordion</h1>
+        <div className="ui styled accordion">
+            {renderedItems}
         </div>
     )
 }
