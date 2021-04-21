678ce803299622c29f2cfc685be3d32fb71363b9
mcshakes <efmccarthy26@gmail.com>
2021-04-21 07:12:40 -0600

    first useEffect waits for a term a user types in. If 1 second goes by and the user hasn't typed (meaning, this is the intended search), that term will be set to state debouncedTerm. Once it is in debouncedTerm, it will be searched in Wikipedia. If a user keeps typing, clearTimeout and timer is reset

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,7 +3,18 @@ import axios from "axios";
 
 const Search = () => {
     const [term, setTerm] = useState("");
+    const [debouncedTerm, setDebouncedTerm] = useState(term);
     const [results, setResults] = useState([]);
+
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedTerm(term);
+        }, 1000);
+
+        return () => {
+            clearTimeout(timerId);
+        };
+    }, [term])
 
     useEffect(() => {
         const fetchWikiData = async () => {
@@ -13,29 +24,49 @@ const Search = () => {
                     list: "search",
                     origin: "*",
                     format: "json",
-                    srsearch: term
+                    srsearch: debouncedTerm
                 }
             })
 
             setResults(data.query.search)
-        }
-        //  above params form => ?action=query&list=search&format=json&srsearch=programming
+        };
 
-        if (term && !results.length) { 
+        if (debouncedTerm) {
             fetchWikiData()
-        } else {
-            const timeoutId = setTimeout(() => {
-                if (term) {
-                    fetchWikiData();
-                }
-            }, 500);
-    
-            return () => {
-                clearTimeout(timeoutId);
-            };
         }
         
-    }, [term, results.length]); // dependency array whines if no results.length. 
+    }, [debouncedTerm]);
+
+    // useEffect(() => {
+    //     const fetchWikiData = async () => {
+    //         const {data} = await axios.get("https://en.wikipedia.org/w/api.php", {
+    //             params: {
+    //                 action: "query",
+    //                 list: "search",
+    //                 origin: "*",
+    //                 format: "json",
+    //                 srsearch: debouncedTerm
+    //             }
+    //         })
+
+    //         setResults(data.query.search)
+    //     };
+
+    //     if (term && !results.length) { 
+    //         fetchWikiData()
+    //     } else {
+    //         const timeoutId = setTimeout(() => {
+    //             if (term) {
+    //                 fetchWikiData();
+    //             }
+    //         }, 1000);
+    
+    //         return () => {
+    //             clearTimeout(timeoutId);
+    //         };
+    //     }
+        
+    // }, [term, results.length]);
 
     const renderedResults = results.map((result) => {
         return (
